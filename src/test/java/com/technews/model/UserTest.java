0af09d28340bc1d4ee2b5a7e3f16ddba5021e75c
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2023-06-07 11:45:00 -0400

    Initial commit

---
+++ src/test/java/com/technews/model/UserTest.java
@@ -0,0 +1,84 @@
+package com.technews.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void isLoggedIn() {
+    }
+
+    @Test
+    void setLoggedIn() {
+    }
+
+    @Test
+    void getPosts() {
+    }
+
+    @Test
+    void setPosts() {
+    }
+
+    @Test
+    void getVotes() {
+    }
+
+    @Test
+    void setVotes() {
+    }
+
+    @Test
+    void getComments() {
+    }
+
+    @Test
+    void setComments() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
